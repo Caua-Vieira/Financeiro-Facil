8cc621b84884b5f3314ca3cbfbbda3da88656040
Caua-Vieira <cauasilva8@gmail.com>
2024-09-10 22:51:39 -0300

    STYLE: Mudança de cores e centralização do menu hamburguer

--- client/src/components/leftBar.tsx
+++ client/src/components/leftBar.tsx
@@ -20,44 +20,49 @@ const LeftBar: React.FC = () => {
                 <div className="row flex-nowrap">
                     <div className={`col-auto col-md-3 col-xl-2 px-sm-2 px-0 bg-white shadow-sm ${isSidebarOpen ? '' : 'sidebar-collapsed'}`}>
                         <div className="d-flex flex-column align-items-center align-items-sm-start px-3 pt-3 min-vh-100">
-                            <div className="d-flex justify-content-between align-items-center pb-3">
+                            <div className="d-flex justify-content-center align-items-center pb-3 w-100">
                                 <img
                                     src="your-logo-url-here"
                                     alt="Logo"
-                                    className={`img-fluid mb-2 ${isSidebarOpen ? '' : 'd-none'}`} // Hide logo if sidebar is collapsed
+                                    className={`img-fluid mb-2 ${isSidebarOpen ? '' : 'd-none'}`}
                                     style={{ width: '150px' }}
                                 />
-                                <button className="btn btn-light" onClick={toggleSidebar}>
+                                <button className="btn btn-light" onClick={toggleSidebar} style={{ marginLeft: 'auto' }}>
                                     <Icon path={mdiMenu} size={1} />
                                 </button>
                             </div>
-                            <ul className="mt-3 nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
-                                <li className="nav-item">
-                                    <a href="#" className={`nav-link ${isSidebarOpen ? 'text-white bg-danger rounded' : 'text-muted'}`}>
-                                        <Icon path={mdiHomeSearchOutline} size={1} /><span className={`w-100 ms-1 ${isSidebarOpen ? 'd-inline' : 'd-none'}`}>Dashboard</span>
+                            <ul className="mt-3 nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start w-100" id="menu">
+                                <li className="nav-item w-100">
+                                    <a href="#" className={`nav-link w-100 ${isSidebarOpen ? 'text-white' : 'text-muted'}`} style={{ backgroundColor: isSidebarOpen ? '#007bff' : 'transparent' }}>
+                                        <Icon path={mdiHomeSearchOutline} size={1} />
+                                        <span className={`ms-1 ${isSidebarOpen ? 'd-inline' : 'd-none'}`}>Dashboard</span>
                                     </a>
                                 </li>
-                                <li className="nav-item">
-                                    <a href="#" className={`nav-link ${isSidebarOpen ? 'text-muted' : 'text-muted'}`}>
-                                        <Icon path={mdiFinance} size={1} /> <span className={`ms-1 ${isSidebarOpen ? 'd-inline' : 'd-none'}`}>Estatísticas</span>
+                                <li className="nav-item w-100">
+                                    <a href="#" className={`nav-link w-100 ${isSidebarOpen ? 'text-muted' : 'text-muted'}`}>
+                                        <Icon path={mdiFinance} size={1} />
+                                        <span className={`ms-1 ${isSidebarOpen ? 'd-inline' : 'd-none'}`}>Estatísticas</span>
                                     </a>
                                 </li>
-                                <li className="nav-item">
-                                    <a href="#" className={`nav-link ${isSidebarOpen ? 'text-muted' : 'text-muted'}`}>
-                                        <Icon path={mdiFileDocumentCheckOutline} size={1} /><span className={`ms-1 ${isSidebarOpen ? 'd-inline' : 'd-none'}`}>Relatórios</span>
+                                <li className="nav-item w-100">
+                                    <a href="#" className={`nav-link w-100 ${isSidebarOpen ? 'text-muted' : 'text-muted'}`}>
+                                        <Icon path={mdiFileDocumentCheckOutline} size={1} />
+                                        <span className={`ms-1 ${isSidebarOpen ? 'd-inline' : 'd-none'}`}>Relatórios</span>
                                     </a>
                                 </li>
-                                <li className="nav-item">
-                                    <a href="#" className={`nav-link ${isSidebarOpen ? 'text-muted' : 'text-muted'}`}>
-                                        <Icon path={mdiCashMultiple} size={1} /><span className={`ms-1 ${isSidebarOpen ? 'd-inline' : 'd-none'}`}>Orçamentos</span>
+                                <li className="nav-item w-100">
+                                    <a href="#" className={`nav-link w-100 ${isSidebarOpen ? 'text-muted' : 'text-muted'}`}>
+                                        <Icon path={mdiCashMultiple} size={1} />
+                                        <span className={`ms-1 ${isSidebarOpen ? 'd-inline' : 'd-none'}`}>Orçamentos</span>
                                     </a>
                                 </li>
                             </ul>
                         </div>
                     </div>
-                    <div className="col py-3">Content area...</div>
+                    {/* <div className="col py-3">Content area...</div> */}
                 </div>
             </div>
+
         </>
     );
 };
