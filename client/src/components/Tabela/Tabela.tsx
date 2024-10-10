0a85d981fc6c0ac16b613f1945753b44e6ce83d5
Caua-Vieira <cauasilva8@gmail.com>
2024-10-09 21:29:32 -0300

    FEAT: Criação do componente de tabela

---
+++ client/src/components/Tabela/Tabela.tsx
@@ -0,0 +1,60 @@
+import { Badge } from "react-bootstrap";
+import { interfaceTable } from "./TabelaInterface";
+import Icon from "@mdi/react";
+import { mdiDelete } from "@mdi/js";
+
+interface TabelaProps {
+    coluna: interfaceTable[];
+    dados: any;
+    usaDelete?: boolean
+    deleteClick?: (dados: any) => void
+}
+
+const Tabela: React.FC<TabelaProps> = ({
+    coluna,
+    dados,
+    usaDelete = false,
+    deleteClick = function () { }
+}) => {
+
+    return (
+        <>
+            <div className="table-responsive">
+                <table className="table table-centered w-100 dt-responsive nowrap table-striped table-hover">
+                    <thead className="table-cabecalho">
+                        {coluna.map((item, index) => (
+                            <th key={index}>{item.titulo}</th>
+                        ))}
+                        <th hidden={!usaDelete}>Ações</th>
+                    </thead>
+                    <tbody>
+                        {dados.map((item: any, index: any) => (
+                            <tr key={index}>
+
+                                {coluna.map((coluna, index) => (
+                                    <td key={index}>
+
+                                        {coluna.titulo === 'renda' ? (
+                                            <Badge bg="success">{coluna.acesso}</Badge>
+                                        ) : (
+                                            item[coluna.acesso]
+                                        )}
+
+                                    </td>
+                                ))}
+                                <td hidden={!usaDelete}>
+                                    <button type="button">
+                                        <Icon path={mdiDelete} size={1} />Excluir
+                                    </button>
+                                </td>
+
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
+    )
+}
+
+export default Tabela
---
+++ client/src/components/Tabela/TabelaInterface.ts
@@ -0,0 +1,4 @@
+export interface interfaceTable {
+    titulo: string;
+    acesso: string;
+}
