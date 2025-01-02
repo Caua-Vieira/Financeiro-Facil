acc4c530898a09d0cf708cc24ddcde2229a5b907
Caua-Vieira <cauasilva8@gmail.com>
2025-01-01 22:54:15 -0300

    FEAT: Criar arquivo de investimentos

---
+++ client/src/pages/dashboard/investimentos/investimentos.tsx
@@ -0,0 +1,17 @@
+function Investimentos() {
+
+    return (
+        <>
+            <h2 className='text-white m-3 '>Investimentos</h2>
+            <div
+                className="p-4 interface-padrao rounded"
+            >
+
+
+
+            </div>
+        </>
+    )
+}
+
+export default Investimentos
